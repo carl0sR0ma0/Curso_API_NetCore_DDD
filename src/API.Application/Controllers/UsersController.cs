7dbf826da6a9701cb96bf43c77c00d9c934f8310
Carlos Romao <cvpromao@gmail.com>
2024-02-12 15:07:09 -0300

    :bricks: Estruturando a camada de Application, criando as referências e aplicando a injeção de dependência.

---
+++ src/API.Application/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _service;
+
+        public UsersController(IUserService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            try
+            {
+                return Ok(await _service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int) HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+    }
+}
---
+++ src/API.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -0,0 +1,17 @@
+﻿using Data.Context;
+using Data.Repository;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CrossCutting.DependencyInjection
+{
+    public class ConfigureRepository
+    {
+        public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddDbContext<MyContext>(options => options.UseNpgsql("Server=127.0.0.1;Port=5432;Pooling=false;Database=dbAPI;User Id=postgres;Password=pentabr0610;"));
+        }
+    }
+}
