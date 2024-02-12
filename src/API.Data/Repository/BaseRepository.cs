461b61f5895fbb35a5cb98290169eaa00d5352a6
Carlos Romao <cvpromao@gmail.com>
2024-02-12 13:55:23 -0300

    :card_file_box: Finalizando a implementação da repository genérica

---
+++ src/API.Data/Repository/BaseRepository.cs
@@ -0,0 +1,105 @@
+﻿using Data.Context;
+using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repository
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly MyContext _context;
+        private DbSet<T> _dataset;
+
+        public BaseRepository(MyContext context)
+        {
+            _context = context;
+            _dataset = _context.Set<T>();
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            try
+            {
+                var result = await _dataset.SingleOrDefaultAsync(e => e.Id.Equals(id));
+                if (result is null) return false;
+
+                _dataset.Remove(result);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<T> InsertAsync(T entity)
+        {
+            try
+            {
+                if (entity.Id == Guid.Empty) entity.Id = Guid.NewGuid();
+
+                entity.CreateAt = DateTime.UtcNow;
+                _dataset.Add(entity);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return entity;
+        }
+
+        public async Task<bool> ExistAsync(Guid id)
+        {
+            return await _dataset.AnyAsync(e => e.Id.Equals(id));
+        }
+
+        public async Task<T> SelectAsync(Guid id)
+        {
+            try
+            {
+                return await _dataset.SingleOrDefaultAsync(e => e.Id.Equals(id));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<T>> SelectAsync()
+        {
+            try
+            {
+                return await _dataset.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            try
+            {
+                var result = await _dataset.SingleOrDefaultAsync(e => e.Id.Equals(entity.Id));
+                if (result is null) return null;
+
+                entity.UpdateAt = DateTime.UtcNow;
+                entity.CreateAt = result.CreateAt;
+
+                _context.Entry(result).CurrentValues.SetValues(entity);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return entity;
+        }
+    }
+}
